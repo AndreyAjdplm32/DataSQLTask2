ccd372ae0c4af8b42b461a2900e47feff020271a
AndreyAndrianov jdplm-32 <darkc0lder@mail.ru>
2023-08-29 20:27:24 +0300

    fix

--- src/Orders.sql
+++ src/Orders.sql
@@ -1,4 +1,9 @@
-SELECT o.product_name AS product
-FROM ORDERS o
-         JOIN CUSTOMERS c ON o.customer_id = c.id
-WHERE LOWER(c.name) = 'alexey';
+CREATE TABLE ORDERS (
+                        id INT AUTO_INCREMENT NOT NULL,
+                        date DATE NOT NULL,
+                        customer_id INT NOT NULL,
+                        product_name VARCHAR(255) NOT NULL,
+                        amount DECIMAL(10, 2) NOT NULL,
+                        PRIMARY KEY (id),
+                        FOREIGN KEY (customer_id) REFERENCES netology_sql_dz.customers(id)
+);
